c181ed69beb5979eaf4d9cd51aaa9db66c122e17
Timothy Ege <retibra@gmail.com>
2018-08-23 13:29:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,16 +5,16 @@ class App < Sinatra::Base
 
   get '/newteam' do
     erb :newteam
-     @name = params[:name]
-     @coach = params[:coach]
-     @pg = params[:pg]
-     @sg = params[:sg]
-     @sf = params[:sf]
-     @pf = params[:pf]
-     @c = params[:c]
   end
 
   post "/team" do
     erb :team
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
   end
 end
